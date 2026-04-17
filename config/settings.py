9474ddcb22ace7af08a039c0ba6c10e9c2f63e0a
Nicholas Arnold <nick@Nicholass-MacBook-Pro.local>
2026-04-17 16:15:36 -0700

    add CORS config

--- config/settings.py
+++ config/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     'players',
     'stats',
     'ingestion',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[])
