c54e789419fd66034c0828e36d381f768a784faf
Nicholas Arnold <nick@Nicholass-MacBook-Pro.local>
2026-04-19 14:54:46 -0700

    fix minutes parsing in materialized view sql

--- stats/sql/player_game_log.sql
+++ stats/sql/player_game_log.sql
@@ -1,43 +1,79 @@
 CREATE MATERIALIZED VIEW player_game_log AS
+WITH cleaned AS (
+    SELECT
+        ps.id,
+        ps.player_id,
+        ps.team_id,
+        ps.game_id,
+        g.season,
+        g.game_date,
+        g.home_team_id,
+        g.away_team_id,
+        g.home_score,
+        g.away_score,
+        ps.minutes,
+        ps.pts,
+        ps.reb,
+        ps.ast,
+        ps.stl,
+        ps.blk,
+        ps.turnover,
+        ps.fgm,
+        ps.fga,
+        ps.fg_pct,
+        ps.fg3m,
+        ps.fg3a,
+        ps.fg3_pct,
+        ps.ftm,
+        ps.fta,
+        ps.ft_pct,
+        ps.oreb,
+        ps.dreb,
+        ps.pf,
+        CASE
+            WHEN ps.minutes IS NULL THEN 0
+            WHEN ps.minutes ~ '^\d+:\d+$' THEN SPLIT_PART(ps.minutes, ':', 1)::integer
+            WHEN ps.minutes ~ '^\d+$' THEN ps.minutes::integer
+            ELSE 0
+        END AS mins
+    FROM stats_playerstat ps
+    JOIN games_game g ON ps.game_id = g.id
+)
 SELECT
-    ps.id AS playerstat_id,
-    ps.player_id,
-    ps.team_id,
-    ps.game_id,
-    g.season,
-    g.game_date,
-    g.home_team_id,
-    g.away_team_id,
-    g.home_score,
-    g.away_score,
-    ps.minutes,
-    ps.pts,
-    ps.reb,
-    ps.ast,
-    ps.stl,
-    ps.blk,
-    ps.turnover,
-    ps.fgm,
-    ps.fga,
-    ps.fg_pct,
-    ps.fg3m,
-    ps.fg3a,
-    ps.fg3_pct,
-    ps.ftm,
-    ps.fta,
-    ps.ft_pct,
-    ps.oreb,
-    ps.dreb,
-    ps.pf
-FROM stats_playerstat ps
-JOIN games_game g ON ps.game_id = g.id
-WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-    AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0
+    id AS playerstat_id,
+    player_id,
+    team_id,
+    game_id,
+    season,
+    game_date,
+    home_team_id,
+    away_team_id,
+    home_score,
+    away_score,
+    minutes,
+    pts,
+    reb,
+    ast,
+    stl,
+    blk,
+    turnover,
+    fgm,
+    fga,
+    fg_pct,
+    fg3m,
+    fg3a,
+    fg3_pct,
+    ftm,
+    fta,
+    ft_pct,
+    oreb,
+    dreb,
+    pf
+FROM cleaned
+WHERE mins > 0
 WITH DATA;
 
-CREATE UNIQUE INDEX player_game_log_idx
-    ON player_game_log (playerstat_id);
-
+CREATE UNIQUE INDEX player_game_log_idx ON player_game_log (playerstat_id);
 CREATE INDEX player_game_log_player_idx ON player_game_log (player_id);
 CREATE INDEX player_game_log_team_idx ON player_game_log (team_id);
 CREATE INDEX player_game_log_season_idx ON player_game_log (season);
--- stats/sql/player_season_stats.sql
+++ stats/sql/player_season_stats.sql
@@ -1,39 +1,50 @@
 CREATE MATERIALIZED VIEW player_season_stats AS
+WITH cleaned AS (
+    SELECT
+        ps.player_id,
+        ps.team_id,
+        g.season,
+        ps.pts,
+        ps.reb,
+        ps.ast,
+        ps.stl,
+        ps.blk,
+        ps.turnover,
+        ps.fgm,
+        ps.fga,
+        ps.fg3m,
+        ps.fg3a,
+        ps.ftm,
+        ps.fta,
+        CASE
+            WHEN ps.minutes IS NULL THEN 0
+            WHEN ps.minutes ~ '^\d+:\d+$' THEN SPLIT_PART(ps.minutes, ':', 1)::integer
+            WHEN ps.minutes ~ '^\d+$' THEN ps.minutes::integer
+            ELSE 0
+        END AS mins
+    FROM stats_playerstat ps
+    JOIN games_game g ON ps.game_id = g.id
+)
 SELECT
-    ps.player_id,
-    ps.team_id,
-    g.season,
-    COUNT(*) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0) AS gp,
-    ROUND(AVG(ps.pts) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0)::numeric, 1) AS ppg,
-    ROUND(AVG(ps.reb) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0)::numeric, 1) AS rpg,
-    ROUND(AVG(ps.ast) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0)::numeric, 1) AS apg,
-    ROUND(AVG(ps.stl) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0)::numeric, 1) AS spg,
-    ROUND(AVG(ps.blk) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0)::numeric, 1) AS bpg,
-    ROUND(AVG(ps.turnover) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0)::numeric, 1) AS topg,
-    ROUND(AVG(ps.fgm::float / NULLIF(ps.fga, 0)) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0)::numeric, 3) AS fg_pct,
-    ROUND(AVG(ps.fg3m::float / NULLIF(ps.fg3a, 0)) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0)::numeric, 3) AS fg3_pct,
-    ROUND(AVG(ps.ftm::float / NULLIF(ps.fta, 0)) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0)::numeric, 3) AS ft_pct,
-    SUM(ps.pts) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0) AS total_pts,
-    SUM(ps.turnover) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0) AS total_tov,
-    SUM(ps.reb) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0) AS total_reb,
-    SUM(ps.ast) FILTER (WHERE REGEXP_REPLACE(ps.minutes, '[^0-9].*', '') ~ '^[0-9]+$'
-        AND REGEXP_REPLACE(ps.minutes, '[^0-9].*', '')::integer > 0) AS total_ast
-FROM stats_playerstat ps
-JOIN games_game g ON ps.game_id = g.id
-GROUP BY ps.player_id, ps.team_id, g.season
+    player_id,
+    team_id,
+    season,
+    COUNT(*) FILTER (WHERE mins > 0) AS gp,
+    ROUND(AVG(pts) FILTER (WHERE mins > 0)::numeric, 1) AS ppg,
+    ROUND(AVG(reb) FILTER (WHERE mins > 0)::numeric, 1) AS rpg,
+    ROUND(AVG(ast) FILTER (WHERE mins > 0)::numeric, 1) AS apg,
+    ROUND(AVG(stl) FILTER (WHERE mins > 0)::numeric, 1) AS spg,
+    ROUND(AVG(blk) FILTER (WHERE mins > 0)::numeric, 1) AS bpg,
+    ROUND(AVG(turnover) FILTER (WHERE mins > 0)::numeric, 1) AS topg,
+    ROUND(AVG(fgm::float / NULLIF(fga, 0)) FILTER (WHERE mins > 0)::numeric, 3) AS fg_pct,
+    ROUND(AVG(fg3m::float / NULLIF(fg3a, 0)) FILTER (WHERE mins > 0)::numeric, 3) AS fg3_pct,
+    ROUND(AVG(ftm::float / NULLIF(fta, 0)) FILTER (WHERE mins > 0)::numeric, 3) AS ft_pct,
+    SUM(pts) FILTER (WHERE mins > 0) AS total_pts,
+    SUM(turnover) FILTER (WHERE mins > 0) AS total_tov,
+    SUM(reb) FILTER (WHERE mins > 0) AS total_reb,
+    SUM(ast) FILTER (WHERE mins > 0) AS total_ast
+FROM cleaned
+GROUP BY player_id, team_id, season
 WITH DATA;
 
 CREATE UNIQUE INDEX player_season_stats_idx
