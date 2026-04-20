361b71d65703a2e3218caa4a3ddeb9aad4984ad5
Nicholas Arnold <nick@Nicholass-MacBook-Pro.local>
2026-04-20 11:18:50 -0700

    add postseason flag to seed_bdl_stats

--- ingestion/management/commands/seed_bdl_stats.py
+++ ingestion/management/commands/seed_bdl_stats.py
@@ -14,14 +14,16 @@ class Command(BaseCommand):
         parser.add_argument('--seasons', nargs='+', type=int)
         parser.add_argument('--start-season', type=int, default=2015)
         parser.add_argument('--end-season', type=int, default=2024)
+        parser.add_argument('--postseason', action='store_true', help='Fetch postseason stats')
 
     def handle(self, *args, **options):
         api_key = settings.BALLDONTLIE_API_KEY
         headers = {'Authorization': api_key}
         base_url = 'https://api.balldontlie.io/v1/stats'
+        postseason = options['postseason']
 
         seasons = options['seasons'] or list(range(options['start_season'], options['end_season'] + 1))
-        self.stdout.write(f'Seeding stats for seasons: {seasons}')
+        self.stdout.write(f'Seeding {"postseason" if postseason else "regular season"} stats for seasons: {seasons}')
 
         for season in seasons:
             self.stdout.write(f'\n--- Season {season} ---')
@@ -30,13 +32,13 @@ class Command(BaseCommand):
 
             while True:
                 params = {'seasons[]': season, 'per_page': 100}
+                if postseason:
+                    params['postseason'] = 'true'
                 if cursor:
                     params['cursor'] = cursor
 
                 self.stdout.write(f'  Fetching cursor: {cursor}')
                 r = requests.get(base_url, headers=headers, params=params)
-
-                
                 data = r.json()
                 stats = data['data']
                 meta = data['meta']
@@ -45,13 +47,9 @@ class Command(BaseCommand):
                     break
 
                 for s in stats:
-                    
                     nba_game_id = str(s['game']['id'])
-                    nba_player_id = s['player']['id']
-
-                    game = Game.objects.filter(nba_game_id=nba_game_id).first()
-                    #player = Player.objects.filter(nba_player_id=nba_player_id).first()
                     player = Player.objects.filter(bdl_player_id=s['player']['id']).first()
+                    game = Game.objects.filter(nba_game_id=nba_game_id).first()
 
                     if not game or not player:
                         continue
